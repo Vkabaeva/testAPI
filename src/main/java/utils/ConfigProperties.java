ca9b4abe838b42b3807cdfffa5dc886c399ab108
Vkabaeva <vkabaeva@punicapp.com>
2018-10-30 08:23:07 +0300

    get property

---
+++ src/main/java/utils/ConfigProperties.java
@@ -0,0 +1,30 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigProperties {
+
+    protected static FileInputStream fileInputStream;
+    protected static Properties properties;
+
+    static {
+        try {
+            fileInputStream = new FileInputStream("src/main/resources/regres.properties");
+            properties = new Properties();
+            properties.load(fileInputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fileInputStream != null)
+                try {
+                fileInputStream.close();
+                } catch (IOException e) {
+            e.printStackTrace();
+            }
+        }
+    }
+
+    public static String getTestProperty(String key) {return properties.getProperty(key);}
+}
